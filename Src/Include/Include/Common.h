3bffd5f77ba808671f17855fc6dde72087e1f2b6
zinoviev <zinoviev.sd@gmail.com>
2023-02-04 21:06:23 +0300

    Добавлен общий для проекта заголовочный файл Common.h

---
+++ Src/Include/Include/Common.h
@@ -0,0 +1,18 @@
+/**
+ * @file  Common.h
+ * @brief Entities common to the entire project.
+ */
+
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+/** Input parameter. */
+#define IN
+
+/** Output parameter. */
+#define OUT
+
+/** Optional parameter. */
+#define OPTIONAL
+
+#endif // __COMMON_H__
