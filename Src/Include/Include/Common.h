0e99ce3e92ca7c90b052c17fbda08312c8afc890
zinoviev <zinoviev.sd@gmail.com>
2023-02-11 16:02:43 +0300

    Добавление макросов для работы со статус кодами

--- Src/Include/Include/Common.h
+++ Src/Include/Include/Common.h
@@ -15,29 +15,64 @@
 //         Modules declaration block for doxygen         //
 ///////////////////////////////////////////////////////////
 
-/** @defgroup MISC            Entities common to all projects */
-/** @defgroup DATA_STRUCTURES Fundamental data structures */
+/** @defgroup  MISC             Entities common to all projects */
+/** @defgroup  DATA_STRUCTURES  Fundamental data structures     */
 
 
 ///////////////////////////////////////////////////////////
-/// @name Direction macros
-///@{
+//         Modules declaration block for doxygen         //
+///////////////////////////////////////////////////////////
 
-/** Input parameter. */
+/** An input parameter in a function or method. */
 #define IN
 
-/** Output parameter. */
+/** An output parameter in a function or method. */
 #define OUT
 
-/** Optional parameter. */
+/** An optional parameter in a function or method/ */
 #define OPTIONAL
-
-///@}
 
 
 ///////////////////////////////////////////////////////////
-/// @name Encapsulation implementation in C
-///@{
+//                    Return statuses                    //
+///////////////////////////////////////////////////////////
+
+/**
+ * @brief Status returned by functions and methods.
+ *
+ * @details The status is uint64_t with the following structure:
+ *
+ *          0x00000000
+ *            ||  |
+ *            Error bit (1 bit), if 1, then error
+ *             |  |
+ *             |  Line (16 bit)
+ *             |
+ *             Status code (12 bit)
+ */
+typedef uint32_t STATUS_CODE;
+
+/** Successful status. */
+#define SC_SUCCESS 0x00000000U
+
+/** The most significant bit indicates an error. */
+#define SC_ERROR_BIT 0x80000000U
+
+#define SC_INVALID_PARAMETER (SC_ERROR_BIT | 0x00010000)
+#define SC_NOT_ENOUGH_MEMORY (SC_ERROR_BIT | 0x00020000)
+#define SC_UNSUCCESSFUL      (SC_ERROR_BIT | 0x00030000)
+
+
+/** Sets the return status to the desired state(StatusCode). */
+#define SET_SC(StatusCode) \
+   status = (0x0000FFFF & __LINE__) | StatusCode
+
+#define SC_ERROR(StatusCode) (int32_t)StatusCode < 0
+
+
+///////////////////////////////////////////////////////////
+//                  Encapsulation in C                   //
+///////////////////////////////////////////////////////////
 
 /** Unique protocol structure ID. Formed using macro STRUCT_ID_64.*/
 typedef uint64_t STRUCT_ID;
@@ -48,9 +83,10 @@ typedef uint64_t STRUCT_ID;
 #define STRUCT_ID_32(A, B, C, D) \
    (STRUCT_ID_16(A, B) | (STRUCT_ID_16(C, D) << 16))
 
-/** Generates a STRUCT_ID.
- *  The first 8 letters of the protocol name must be given as input.
- *  For correct operation, similar protocol names should be avoided.
+/**
+ * Generates a STRUCT_ID. The first 8 letters of the protocol name
+ * must be given as input. For correct operation, similar protocol
+ * names should be avoided.
  */
 #define STRUCT_ID_64(A, B, C, D, E, F, G, H) \
    (STRUCT_ID_32(A, B, C, D) | ((uint64_t)STRUCT_ID_32(E, F, G, H) << 32))
@@ -72,8 +108,5 @@ typedef uint64_t STRUCT_ID;
 #define GET_THIS(This, ClassName) \
    ClassName* this = GET_STRUCT_FIELD(This, ClassName, VTable); \
    if (this->StructureId != ClassName ## _STRUCT_ID) this = NULL;
-
-///@}
-
 
 #endif // __MISC_H__
